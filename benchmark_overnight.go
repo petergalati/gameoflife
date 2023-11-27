a3a1b48b9f4fc083259cb52af19dbea65ea154ea
nic <os22128@bristol.ac.uk>
2023-11-27 15:40:37 +0000

    adding overnight benchmarking

---
+++ benchmark_overnight.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"uk.ac.bris.cs/gameoflife/gol"
+)
+
+const benchLength = 1000
+
+func BenchmarkGol(b *testing.B) {
+	sizes := [6]int{16, 64, 128, 256, 512, 5120}
+	for i := range sizes {
+		os.Stdout = nil // Disable all program output apart from benchmark results
+		p := gol.Params{
+			Turns:       benchLength,
+			Threads:     1,
+			ImageWidth:  sizes[i],
+			ImageHeight: sizes[i],
+		}
+		name := fmt.Sprintf("%dx%dx%d-%d", p.ImageWidth, p.ImageHeight, p.Turns, p.Threads)
+		b.Run(name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				events := make(chan gol.Event)
+				go gol.Run(p, events, nil)
+				for range events {
+
+				}
+			}
+		})
+	}
+
+	for i := range sizes {
+		os.Stdout = nil // Disable all program output apart from benchmark results
+		p := gol.Params{
+			Turns:       benchLength,
+			Threads:     8,
+			ImageWidth:  sizes[i],
+			ImageHeight: sizes[i],
+		}
+		name := fmt.Sprintf("%dx%dx%d-%d", p.ImageWidth, p.ImageHeight, p.Turns, p.Threads)
+		b.Run(name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				events := make(chan gol.Event)
+				go gol.Run(p, events, nil)
+				for range events {
+
+				}
+			}
+		})
+	}
+	for i := range sizes {
+		os.Stdout = nil // Disable all program output apart from benchmark results
+		p := gol.Params{
+			Turns:       benchLength,
+			Threads:     16,
+			ImageWidth:  sizes[i],
+			ImageHeight: sizes[i],
+		}
+		name := fmt.Sprintf("%dx%dx%d-%d", p.ImageWidth, p.ImageHeight, p.Turns, p.Threads)
+		b.Run(name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				events := make(chan gol.Event)
+				go gol.Run(p, events, nil)
+				for range events {
+
+				}
+			}
+		})
+	}
+
+	for i := 0; i > 100; i++ {
+		os.Stdout = nil // Disable all program output apart from benchmark results
+		p := gol.Params{
+			Turns:       benchLength,
+			Threads:     i,
+			ImageWidth:  512,
+			ImageHeight: 512,
+		}
+		name := fmt.Sprintf("%dx%dx%d-%d", p.ImageWidth, p.ImageHeight, p.Turns, p.Threads)
+		b.Run(name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				events := make(chan gol.Event)
+				go gol.Run(p, events, nil)
+				for range events {
+
+				}
+			}
+		})
+	}
+}
